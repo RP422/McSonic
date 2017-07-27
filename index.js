8d4c5de3ab03f17a21f7861ace166c82e933827d
Mike Lang <mlang@student.neumont.edu>
2017-07-27 08:18:02 -0600

    - Added the beginning of menu.pug - Filled in some of the basis of index.js
    Signed-off-by: Mike Lang <mlang@student.neumont.edu>

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express'), 
+		pug = require('pug'),
+		path = require('path'),
+		config = require('./config');
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/:page', function(req, res) {
+	res.render(req.params.page, 
+						 {title: req.params.page,
+							config: config
+						 });
+});
+
+app.listen(3000);
