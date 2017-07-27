97a2750c4686f336a9fbfeaffb4ca00517f1c7fa
Mike Lang <mlang@student.neumont.edu>
2017-07-27 08:47:52 -0600

    - Tinkered with my menu backend: changed the path slightly and fixed URL param stuffs
    Signed-off-by: Mike Lang <mlang@student.neumont.edu>

--- index.js
+++ index.js
@@ -9,21 +9,22 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public')));
 
-app.get('/:menuCategory', function(req, res) {
+app.get('/menu/:menuCategory', function(req, res) {
 	
 	var submenu;
 	
-	if(req.params.page == "burgers") {
+	if(req.params.menuCategory == "burgers") {
 		submenu = config.menu.burgers;
 	}
-	else if (req.params.page == "sides") {
+	else if (req.params.menuCategory == "sides") {
 		submenu = config.menu.sides;
 	}
-	else if (req.params.page == "desserts") {
-		submenu = config.menu.desserts
+	else if (req.params.menuCategory == "desserts") {
+		submenu = config.menu.desserts;
 	}
 	else {
 		// 404 redirect
+		throw '404 - Page not found';
 	}
 	
 	res.render(req.params.page, 
