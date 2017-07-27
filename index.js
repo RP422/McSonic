20c5f2a9fdfc6a7847a50baf37c388b20afbb556
Mason <lawrencemason00@gmail.com>
2017-07-27 08:27:03 -0600

    basic layout

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    pug = require('pug'),
+    path = require('path'),
+    config = require('./config');
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', function(req, res){
+  res.render('index', {title: "Homepage", config: config});
+});
+
+app.get('/:page', function(req, res){
+  console.log(req.params.page)
+  res.render(req.params.page, {title: req.params.page, config: config})
+});
+
+app.listen(3000);
